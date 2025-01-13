979a289c1fc061fc9c04c976696e03471aa33992
LindeSchoenmaker <l.schoenmaker@lacdr.leidenuniv.nl>
2025-01-13 12:22:06 +0100

    add argparse to analysis scripts

--- rhfe_analysis/get_convergence.py
+++ rhfe_analysis/get_convergence.py
@@ -1,14 +1,39 @@
+import argparse
 import glob
 import json
 import os
 from collections import defaultdict
 
-import numpy as np
-
 import alchemlyb
-from alchemlyb import concat as alchemlyb_concat
+import numpy as np
 from alchemlyb.estimators import MBAR
 from alchemlyb.parsing.gmx import extract_u_nk
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p",
+                    "--path",
+                    help="directory containing dhdl.xvg files",
+                    type=str,
+                    default="workpath")
+parser.add_argument("-c",
+                    "--condition",
+                    help="RHFE or RBFE, which legs to compare",
+                    type=str,
+                    choices=['rhfe', 'rbfe'])
+parser.add_argument("-pl",
+                    "--plot",
+                    help="plot convergence figure",
+                    type=bool,
+                    default = False)
+parser.add_argument("-r",
+                    "--runs",
+                    help="which runs to use",
+                    default=['run1', 'run2', 'run3'],
+                    nargs='+')
+parser.add_argument("-e",
+                    "--edges",
+                    help="which edges to use",
+                    nargs='+')
 
 
 def get_dg(df):
@@ -24,37 +49,55 @@ def get_estimate_uncertainty(data_list, slice_fw = -1, slice_bw = 0):
 
 
 if __name__ == "__main__":
-    base_path = 'workpath_AB_B_2.1.1/results'
-    edges = [os.path.basename(x) for x in glob.glob(f'{base_path}/*')]
+    args = parser.parse_args()
+    base_path = args.path
+
+    if args.edges:
+        edges = args.edges
+    else:
+        edges = [os.path.basename(x) for x in glob.glob(f'{base_path}/*') if 'edge' in x]
+    print(edges)
+
+    condition = args.condition # ['vacuum', 'water']
+
+    if condition == 'rhfe':
+        legs = ['water', 'vacuum']
+
+    elif condition == 'rbfe':
+        legs = ['protein', 'water']
+
+    runs = args.runs
 
     conv_dict = defaultdict(dict)
     conv_std_dict = defaultdict(dict)
 
     interval = 10
+    max_change = 0.1 #kcal/mol
 
     for edge in edges:
+        print(edge)
         forward = defaultdict(list)
         forward_error = defaultdict(list)
         forward_convergence = []
-        for leg in ['vacuum', 'water']:
+        for leg in legs:
             forward_convergence = []
             numbers = [os.path.basename(x) for x in glob.glob(f'{base_path}/{edge}/{leg}/*')]
-            for run in ['1', '2', '3']:
+            for run in runs:
                 last_value = None
-                data_AB = [glob.glob(f'{base_path}/{edge}/{leg}/{number}/run{run}/production/dhdl.xvg')[0] for number in numbers if int(number) < 15]
+                data_AB = [glob.glob(f'{base_path}/{edge}/{leg}/{number}/{run}/production/dhdl.xvg')[0] for number in numbers if int(number) < 15]
                 data_list_AB = [extract_u_nk(xvg, T=298.15) for xvg in data_AB]
 
-                data_B = [glob.glob(f'{base_path}/{edge}/{leg}/{number}/run{run}/production/dhdl.xvg')[0] for number in numbers if int(number) >= 15]
+                data_B = [glob.glob(f'{base_path}/{edge}/{leg}/{number}/{run}/production/dhdl.xvg')[0] for number in numbers if int(number) >= 15]
                 data_list_B = [extract_u_nk(xvg, T=298.15) for xvg in data_B]
 
-                mbar_AB = MBAR().fit(alchemlyb_concat(data_list_AB))
-                mbar_B = MBAR().fit(alchemlyb_concat(data_list_B))
+                # mbar_AB = MBAR().fit(alchemlyb_concat(data_list_AB))
+                # mbar_B = MBAR().fit(alchemlyb_concat(data_list_B))
 
-                dg_AB = get_dg(mbar_AB.delta_f_)
-                dg_B = get_dg(mbar_B.delta_f_)
+                # dg_AB = get_dg(mbar_AB.delta_f_)
+                # dg_B = get_dg(mbar_B.delta_f_)
 
-                final_dg = dg_AB + dg_B
-                err = get_dg(mbar_AB.d_delta_f_) + get_dg(mbar_B.d_delta_f_)
+                # final_dg = dg_AB + dg_B
+                # err = get_dg(mbar_AB.d_delta_f_) + get_dg(mbar_B.d_delta_f_)
                 count = 0
                 for i in range(1, int(len(data_list_AB[0])/interval) + 1):
                     # Do the forward
@@ -65,7 +108,7 @@ if __name__ == "__main__":
                     forward_error[leg + run].append(forward_error_AB + forward_error_B)
                     curr_value = forward_AB + forward_B
                     if last_value:
-                        if abs(last_value-curr_value) < err:
+                        if abs(last_value-curr_value) < max_change:
                             count += 1
                         else:
                             count = 0
@@ -73,13 +116,16 @@ if __name__ == "__main__":
                         if count >= 20: #converged if stays within window for 10 * interval
                             forward_convergence.append(i - 20)
                             break
+                        if i == int(len(data_list_AB[0])/interval):
+                            forward_convergence.append(i - count)
+                            break
                     else:
                         last_value = curr_value
             conv_dict[edge][leg] =  sum(forward_convergence)/len(forward_convergence)
             conv_std_dict[edge][leg] =  np.std(forward_convergence)
         
-    with open("conv.json", "w") as outfile: 
+    with open(f"conv_{condition}.json", "w") as outfile: 
         json.dump(conv_dict, outfile)
 
-    with open("conv_std.json", "w") as outfile: 
+    with open(f"conv_std_{condition}.json", "w") as outfile: 
         json.dump(conv_std_dict, outfile)
---
+++ rhfe_analysis/get_ddg_new.py
@@ -0,0 +1,150 @@
+import argparse
+import glob
+import json
+import math
+import os
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+from alchemlyb import concat as alchemlyb_concat
+from alchemlyb.convergence import forward_backward_convergence
+from alchemlyb.estimators import MBAR
+from alchemlyb.parsing.gmx import extract_u_nk
+from alchemlyb.visualisation import plot_convergence, plot_mbar_overlap_matrix
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p",
+                    "--path",
+                    help="directory containing dhdl.xvg files",
+                    type=str,
+                    default="workpath")
+parser.add_argument("-c",
+                    "--condition",
+                    help="RHFE or RBFE, which legs to compare",
+                    type=str,
+                    choices=['rhfe', 'rbfe'])
+parser.add_argument("-pl",
+                    "--plot",
+                    help="plot convergence figure",
+                    type=bool,
+                    default = False)
+parser.add_argument("-mid",
+                    "--mid",
+                    help="calc ddg halfway through production run",
+                    type=bool,
+                    default = False)
+parser.add_argument("-r",
+                    "--runs",
+                    help="which runs to use",
+                    default=['run1', 'run2', 'run3'],
+                    nargs='+')
+parser.add_argument("-e",
+                    "--edges",
+                    help="which edges to use",
+                    nargs='+')
+
+
+def get_dg(df):
+    """Read dg from result table and convert from kJ to kCal"""
+    dg_kJ = df.iloc[0, len(df)-1]
+    df_kCal = dg_kJ / 4.2
+    return df_kCal
+
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    dg_dict = defaultdict(lambda: defaultdict(dict))
+    ddg_dict = {}
+    ddg_err_dict = {}
+    ddg_mbar_err_dict = {}
+
+    dg_err_dict = defaultdict(lambda: defaultdict(dict))
+
+    base_path = args.path
+    if args.edges:
+        edges = args.edges
+    else:
+        edges = [os.path.basename(x) for x in glob.glob(f'{base_path}/*') if 'edge' in x]
+    print(edges)
+    condition = args.condition # ['vacuum', 'water']
+    if condition == 'rhfe':
+        legs = ['water', 'vacuum']
+    elif condition == 'rbfe':
+        legs = ['protein', 'water']
+
+
+    plot = args.plot
+    runs = args.runs
+
+    for edge in edges:
+        for leg in legs:
+            for run in runs:
+                print(edge, run, leg)
+                numbers = [os.path.basename(x) for x in glob.glob(f'{base_path}/{edge}/{leg}/*')]
+                data_AB = [glob.glob(f'{base_path}/{edge}/{leg}/{number}/{run}/production/dhdl.xvg')[0] for number in numbers if int(number) < 15]
+                if args.mid:
+                    data_list_AB = [extract_u_nk(xvg, T=298.15).iloc[:round(len(extract_u_nk(xvg, T=298.15))/2)] for xvg in data_AB]
+                else:
+                    data_list_AB = [extract_u_nk(xvg, T=298.15) for xvg in data_AB]
+                mbar_AB = MBAR().fit(alchemlyb_concat(data_list_AB))
+                print('B')
+                data_B = [glob.glob(f'{base_path}/{edge}/{leg}/{number}/{run}/production/dhdl.xvg')[0] for number in numbers if int(number) >= 15]
+                if args.mid:
+                    data_list_B = [extract_u_nk(xvg, T=298.15).iloc[:round(len(extract_u_nk(xvg, T=298.15))/2)] for xvg in data_B]
+                else:
+                    data_list_B = [extract_u_nk(xvg, T=298.15) for xvg in data_B]
+                mbar_B = MBAR().fit(alchemlyb_concat(data_list_B))
+
+                dg_AB = get_dg(mbar_AB.delta_f_)
+                dg_B = get_dg(mbar_B.delta_f_)
+                dg = dg_AB + dg_B
+                dg_dict[edge][leg][run] = (dg_AB, dg_B, dg)
+
+                err = get_dg(mbar_AB.d_delta_f_) + get_dg(mbar_B.d_delta_f_)
+                dg_err_dict[edge][leg][run] = err
+
+            if plot:
+                df = forward_backward_convergence(data_list_AB, 'mbar')
+                fig, axs = plt.subplots(2, 2, figsize=(16,9))
+                plot_convergence(df, ax=axs[0,0], units = 'kJ/mol')
+                plot_mbar_overlap_matrix(mbar_AB.overlap_matrix, ax=axs[0,1])
+
+                df = forward_backward_convergence(data_list_B, 'mbar')
+                plot_convergence(df, ax=axs[1,0], units = 'kJ/mol')
+                plot_mbar_overlap_matrix(mbar_B.overlap_matrix, ax=axs[1,1])
+
+                if not os.path.exists('figure'):
+                    os.makedirs('figure')
+                fig.savefig(f'figure/{edge}_{leg}.png')
+            dg_dict[edge][leg]['average'] =  sum([dg_dict[edge][leg][run][-1] for run in runs])/len(runs)
+            dg_dict[edge][leg]['sd'] =  np.std([dg_dict[edge][leg][run][-1] for run in runs])
+            dg_err_dict[edge][leg]['average'] =  sum([dg_err_dict[edge][leg][run] for run in runs])/len(runs)
+
+        ddg_dict[edge] = dg_dict[edge][legs[0]]['average'] - dg_dict[edge][legs[1]]['average']
+        ddg_err_dict[edge] = math.sqrt(dg_dict[edge][legs[0]]['sd']**2 + dg_dict[edge][legs[1]]['sd']**2)
+        ddg_mbar_err_dict[edge] = dg_err_dict[edge][legs[0]]['average'] + dg_err_dict[edge][legs[1]]['average']
+
+
+    print(ddg_dict)
+    print(ddg_err_dict)
+    print(ddg_mbar_err_dict)
+    print(dg_dict)
+    print(dg_err_dict)
+
+    with open(f"ddg_err_{condition}{'_mid' if args.mid else ''}.json", "w") as outfile: 
+        json.dump(ddg_err_dict, outfile)
+
+    with open(f"ddg_{condition}{'_mid' if args.mid else ''}.json", "w") as outfile: 
+        json.dump(ddg_dict, outfile)
+
+    with open(f"ddg_mbar_err_{condition}{'_mid' if args.mid else ''}.json", "w") as outfile: 
+        json.dump(ddg_mbar_err_dict, outfile)
+
+    with open(f"dg_{condition}{'_mid' if args.mid else ''}.json", "w") as outfile: 
+        json.dump(dg_dict, outfile)
+
+    with open(f"dg_err_{condition}{'_mid' if args.mid else ''}.json", "w") as outfile: 
+        json.dump(dg_err_dict, outfile)
